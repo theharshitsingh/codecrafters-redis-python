9fe9ffd5da31c9b17b78fcf8256cb189fe5492b9
theharshitsingh <harshit.singh2030@gmail.com>
2024-04-30 18:58:43 +0530

    stage 3

--- app/main.py
+++ app/main.py
@@ -3,22 +3,25 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-    pong  = "+PONG\r\n"
+    pong = "+PONG\r\n"
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
+    conn, addr = server_socket.accept()  # wait for client
 
     with conn:
         while True:
             data = conn.recv(1024)
-            print(data)
+            print(f"Received: {data}")  # Debugging: log received data
             if not data:
-                break
-            if data.strip() == b'PING':
+                print("No data received, closing connection.")
+                break  # Exit the loop if no data is received, indicating the client has closed the connection
+            data = data.strip()
+            if data == b'PING':
+                print("Sending PONG")
                 conn.send(pong.encode())
-
+            else:
+                print(f"Unexpected data received: {data}")
 
 if __name__ == "__main__":
     main()
