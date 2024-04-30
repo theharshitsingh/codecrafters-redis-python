d5c421ac88d6ebbe9f21a5a61ec038224f10f0a0
theharshitsingh <harshit.singh2030@gmail.com>
2024-04-30 19:06:51 +0530

    stage 3

--- app/main.py
+++ app/main.py
@@ -7,21 +7,16 @@ def main():
     pong = "+PONG\r\n"
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept()  # wait for client
+    client_socket, address = server_socket.accept()
 
-    with conn:
+    with client_socket:
         while True:
-            data = conn.recv(1024)
-            print(f"Received: {data}")  # Debugging: log received data
-            if not data:
-                print("No data received, closing connection.")
-                break  # Exit the loop if no data is received, indicating the client has closed the connection
-            data = data.strip()
-            if data == b'PING':
-                print("Sending PONG")
-                conn.send(pong.encode())
-            else:
-                print(f"Unexpected data received: {data}")
+            try:
+                data = client_socket.recv(1024)
+                if data:
+                    client_socket.sendall(b"+PONG\r\n")
+            except ConnectionResetError:
+                print(f"Connection to {address} lost")
 
 if __name__ == "__main__":
     main()
